eee3b5b9e72fb51a3c30e3f1487c975ebc9032b3
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-02-25 20:37:29 -0800

    Updates

---
+++ precache-manifest.edce5843b2cfb8ab77b17f2d2833fcfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c9633621124cc59c84",
+    "url": "/front-end-project-week/static/js/runtime~main.74c96336.js"
+  },
+  {
+    "revision": "2024ccfdd383f1cec8b0",
+    "url": "/front-end-project-week/static/js/main.2024ccfd.chunk.js"
+  },
+  {
+    "revision": "f050e7926f4a9b977fec",
+    "url": "/front-end-project-week/static/js/2.f050e792.chunk.js"
+  },
+  {
+    "revision": "2024ccfdd383f1cec8b0",
+    "url": "/front-end-project-week/static/css/main.68c7dfbe.chunk.css"
+  },
+  {
+    "revision": "503253040b0749b9a7a0b1187068f8d5",
+    "url": "/front-end-project-week/index.html"
+  }
+];
